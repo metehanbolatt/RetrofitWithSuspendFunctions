b95f0781c10a7d5377c2aa141962cca80f2cd32a
Metehan BOLAT <73827610+metehanie@users.noreply.github.com>
2022-01-20 00:12:50 +0300

    Create RetrofitInstance Object (Singleton)

--- app/src/main/java/com/metehanbolat/retrofitwithsuspendfunctions/api/SimpleApi.kt
+++ app/src/main/java/com/metehanbolat/retrofitwithsuspendfunctions/api/SimpleApi.kt
@@ -1,4 +1,10 @@
 package com.metehanbolat.retrofitwithsuspendfunctions.api
 
+import com.metehanbolat.retrofitwithsuspendfunctions.model.Post
+import retrofit2.http.GET
+
 interface SimpleApi {
+
+    @GET("posts/1")
+    suspend fun getPost(): Post
 }
---
+++ app/src/main/java/com/metehanbolat/retrofitwithsuspendfunctions/utils/Constants.kt
@@ -0,0 +1,8 @@
+package com.metehanbolat.retrofitwithsuspendfunctions.utils
+
+class Constants {
+
+    companion object {
+        const val BASE_URL = "https://jsonplaceholder.typicode.com"
+    }
+}
